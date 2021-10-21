3f8d54b4ce60996308424d860a9ae8605070788c
RemyChazelas <remychazelas@gmail.com>
2021-10-21 23:05:46 +0200

    first

---
+++ index.js
@@ -0,0 +1,93 @@
+document.addEventListener('DOMContentLoaded', function()
+{
+    let fruits = ["Fraise", "Pomme", "Orange", "Poire"]
+
+
+    // 1 - Ajouter "Framboise" à la fin du tableau et "Ananas" au début du tableau
+    //     Afficher le resultat dans la console
+
+
+
+    // 2 - Trouve l'index de "Orange" dans le tableau
+    //     Afficher le resultat dans la console
+
+
+
+    // 3 - Trouve si oui ou non "Poire" est dans le tableau
+    //     Afficher le resultat dans la console
+
+
+
+    // 4 - Return un tableau filtré par les valeurs qui contiennent au moins un "a"
+    //     Afficher le resultat dans la console
+
+    
+
+    // 5 - Parcourir le tableau pour trouver la valeur qui a la plus grande longueur
+    //     Afficher le resultat dans la console
+
+
+
+    // 6 - Retirer "Pomme" du tableau
+    //     Afficher le resultat dans la console
+
+
+
+    let persons = [
+        {
+
+            id: '6124c290e788efa315a2b714',
+            firstname: 'Billy',
+            lastname: 'Morningman',
+            age: 28,
+            job: 'Serrurier',
+            city: 'Roubaix'
+        
+        },
+        {
+
+            id: '609ea3f88d7e1de79edc1b92',
+            firstname: 'Willy',
+            lastname: 'Dixon',
+            age: 35,
+            job: 'Avocat',
+            city: 'Lomme'
+        
+        },
+        {
+
+            id: '6124c290e788efa315a2b714',
+            firstname: 'Benjamin',
+            lastname: 'Franklin',
+            age: 315,
+            job: 'Père Fondateur',
+            city: 'New York'
+        
+        },
+    ]
+
+    // 7 - Récupérér le troisième objet dans le tableau persons et le stocker dans la variable "let person"
+    //     Trouve si l'objet récupéré contient la propriété birthdate
+    //     Afficher le resultat dans la console
+
+    let person
+
+
+    // 8 - Return un tableau des id à partir du tableau d'objet persons
+    //     Afficher le resultat dans la console
+
+
+
+    // 9 - Return l'index de la propriété age dans l'objet person
+    //     Afficher le resultat dans la console
+
+
+
+    // 10 - Trier le tableau persons dans l'ordre alphabétique de la clé firstname
+    //     Afficher le resultat dans la console
+
+
+
+
+
+})
